fa1e76377eb4e2f42a8f30e24bf0c63fb3c31d38
o.pshenichnikova <a.pshenichnikov@be-interactive.ru>
2017-12-12 10:53:50 +0300

    -

--- index.php
+++ index.php
@@ -8,70 +8,74 @@ use Symfony\Component\Yaml\Yaml;
 $config = Yaml::parseFile('config.yml');
 $telegram = new Api($config['token']);
 
-$result = $telegram->getWebhookUpdates();
+try {
+	$result = $telegram->getWebhookUpdates();
 
-//Текст сообщения
-$text = $result["message"]["text"];
-//Уникальный идентификатор пользователя
-$chat_id = $result["message"]["chat"]["id"];
-//Юзернейм пользователя
-$name = $result["message"]["from"]["username"];
-//Клавиатура
-$keyboard = [
-	["Кошке нужна валерьянка"],
-	["Мишка )"]
-];
+	//Текст сообщения
+	$text = $result["message"]["text"];
+	//Уникальный идентификатор пользователя
+	$chat_id = $result["message"]["chat"]["id"];
+	//Юзернейм пользователя
+	$name = $result["message"]["from"]["username"];
+	//Клавиатура
+	$keyboard = [
+		["Кошке нужна валерьянка"],
+		["Мишка )"]
+	];
 
-if($text) {
-	 if ($text == "/start") {
-		$reply = "Добро пожаловать в КошкинБот!";
-		$reply_markup = $telegram->replyKeyboardMarkup([ 
-			'keyboard' => $keyboard, 
-			'resize_keyboard' => true, 
-			'one_time_keyboard' => false 
-		]);
-		
+	if($text) {
+		 if ($text == "/start") {
+			$reply = "Добро пожаловать в КошкинБот!";
+			$reply_markup = $telegram->replyKeyboardMarkup([ 
+				'keyboard' => $keyboard, 
+				'resize_keyboard' => true, 
+				'one_time_keyboard' => false 
+			]);
+
+			$telegram->sendMessage([ 
+				'chat_id' => $chat_id, 
+				'text' => $reply, 
+				'reply_markup' => $reply_markup 
+			]);
+		} elseif ($text == "/help") {
+			$reply = "Слава Котам!!!";
+			$telegram->sendMessage([ 
+				'chat_id' => $chat_id, 
+				'text' => $reply 
+			]);
+		} elseif ($text == "Кошке нужна валерьянка") {
+			$telegram->sendSticker([ 
+				'chat_id' => $chat_id, 
+				'sticker' => 'CAADBAADxgMAAv4zDQY6bEeD67rtlAI'
+			]);
+		} elseif ($text == "Мишка )") {
+			$telegram->sendSticker([ 
+				'chat_id' => $chat_id, 
+				'sticker' => 'CAADAgADnQQAAmvEygrzEw25pNCS5wI'
+			]);
+		} else {
+			$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
+			$telegram->sendMessage([ 
+				'chat_id' => $chat_id, 
+				'parse_mode'=> 'HTML', 
+				'text' => $reply 
+			]);
+		}
+	}else{
 		$telegram->sendMessage([ 
 			'chat_id' => $chat_id, 
-			'text' => $reply, 
-			'reply_markup' => $reply_markup 
+			'text' => "Отправьте текстовое сообщение." 
 		]);
-	} elseif ($text == "/help") {
-		$reply = "Слава Котам!!!";
+
+		/*$debug = print_r($result, true);
+
 		$telegram->sendMessage([ 
 			'chat_id' => $chat_id, 
-			'text' => $reply 
-		]);
-	} elseif ($text == "Кошке нужна валерьянка") {
-		$telegram->sendSticker([ 
-			'chat_id' => $chat_id, 
-			'sticker' => 'CAADBAADxgMAAv4zDQY6bEeD67rtlAI'
-		]);
-	} elseif ($text == "Мишка )") {
-		$telegram->sendSticker([ 
-			'chat_id' => $chat_id, 
-			'sticker' => 'CAADAgADnQQAAmvEygrzEw25pNCS5wI'
-		]);
-	} else {
-		$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-		$telegram->sendMessage([ 
-			'chat_id' => $chat_id, 
-			'parse_mode'=> 'HTML', 
-			'text' => $reply 
-		]);
+			'parse_mode' => 'Markdown', 
+			'disable_web_page_preview' => true, 
+			'text' => "```$debug```"
+		]);*/
 	}
-}else{
-	$telegram->sendMessage([ 
-		'chat_id' => $chat_id, 
-		'text' => "Отправьте текстовое сообщение." 
-	]);
-	
-	/*$debug = print_r($result, true);
-	
-	$telegram->sendMessage([ 
-		'chat_id' => $chat_id, 
-		'parse_mode' => 'Markdown', 
-		'disable_web_page_preview' => true, 
-		'text' => "```$debug```"
-	]);*/
+} catch(Exception $e) {
+	error_log($e->getMessage());
 }
